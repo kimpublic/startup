e2eca0d30c51f22379b8b7a0bd84c1158a1b0a07
kimpublic <minjoongkim98@naver.com>
2025-02-09 03:09:32 -0700

    리액트 파트 1 ~23:33 지금 router 처리함

--- src/app.jsx
+++ src/app.jsx
@@ -2,59 +2,82 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Halloffame } from './halloffame/halloffame';
+import { About } from './about/about';
+import { How } from './how/how';
+import { Invite } from './invite/invite';
+
+
+
 export default function App() {
     return (
-        <div className='body bg-dark text-light'>
-            <header>
-                <br />
-                <div className="logo">
-                    <img src="\sources\sources/sauce/title.png" alt="Logo" width="100%" />     
-                </div>
-        
-                
-                <h2>"Sir, would you like to play a game with me?"</h2>
-                <br />
-                
-                <nav>
-                    <menu className='navvar-nav'>
-                        <li className='nav-item'>
-                            <a className='nav-link' href="index.html">
-                                Home
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href="how.html">
-                                How to Play
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href="play.html">
-                                Play
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href="halloffame.html">
-                                Hall of Fame
-                            </a>
-                        </li>
-                        <li className='nav-item'>
-                            <a className='nav-link' href="about.html">
-                                About
-                            </a>
-                        </li>
-                    </menu>
-                </nav>
-                <br />
-            </header>
+        <BrowserRouter>
+            <div className='body bg-dark text-light'>
+                <header>
+                    <br />
+                    <div className="logo">
+                        <img src="\sources\sources/sauce/title.png" alt="Logo" width="100%" />     
+                    </div>
+            
+                    
+                    <h2>"Sir, would you like to play a game with me?"</h2>
+                    <br />
+                    
+                    <nav>
+                        <menu className='navbar-nav'>
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to="/">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to="how">
+                                    How to Play
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to="play">
+                                    Play
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to="halloffame">
+                                    Hall of Fame
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to="about">
+                                    About
+                                </NavLink>
+                            </li>
+                        </menu>
+                    </nav>
+                    <br />
+                </header>
 
-            <main className='container-fluid bg-secondary text-center'>
-                App components go here
-            </main>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/how' element={<How />} />
+                    <Route path='/play' element={<Play />} />
+                    <Route path='/halloffame' element={<Halloffame />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/invite' element={<Invite />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-            <footer>
-                <span className="text-reset">Author Name: Minjoong Kim</span>
-                <a href="https://github.com/kimpublic/startup">GitHub</a>
-            </footer>
-        </div>
+                <footer>
+                    <span className="text-reset">Author Name: Minjoong Kim</span>
+                    <a href="https://github.com/kimpublic/startup">GitHub</a>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
+}
+
+
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
